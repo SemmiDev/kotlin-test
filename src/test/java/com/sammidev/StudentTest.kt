9153b58a72f3b430c3aa1a55354f4494dc9a2f0b
SemmiDev <sammidev4@gmail.com>
2020-12-24 20:52:46 +0700

    time out

--- src/test/java/com/sammidev/StudentTest.kt
+++ src/test/java/com/sammidev/StudentTest.kt
@@ -4,8 +4,11 @@ import com.sammidev.entity.Student
 import com.sammidev.resolver.StudentResolver
 import com.sammidev.service.StudentService
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.Extensions
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
 import kotlin.test.assertEquals
 
 @Extensions(value = [
@@ -19,5 +22,14 @@ class StudentTest {
     fun addStudent(student: Student) {
         this.student.saveStudent(student)
         assertEquals(student, this.student.findStudentById(1))
+    }
+}
+
+
+class TimeOut {
+    @Test
+    @Timeout(value = 5, unit = SECONDS)
+    fun slow() {
+        Thread.sleep(7000)
     }
 }
